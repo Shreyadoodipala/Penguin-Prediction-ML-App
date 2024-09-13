28ffd07cc13b84319328ed5eac8ddf2368e1fcc1
Shreya Doodipala <95224610+Shreyadoodipala@users.noreply.github.com>
2024-09-13 22:07:16 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,11 @@ with st.sidebar:
        'flipper_length_mm', flipper_length_mm,
        'body_mass_g', body_mass_g,
        'gender', gender}
-  input_df = pd.DataFrame(data, index=0)
+  input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X], axis=1)
 
-input_penguins
+with st.expander('Input features'):
+  st.write('**input penguin**')
+  input_df
+  st.write('**Combined data**')
+  input_penguins
