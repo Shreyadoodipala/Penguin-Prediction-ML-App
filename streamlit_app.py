bb680e3e7b13b9b933cbdd2aebd182dc312b96ec
Shreya Doodipala <95224610+Shreyadoodipala@users.noreply.github.com>
2024-09-13 22:35:12 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,9 @@ target_mapper = {'Adelie':0, 'Chinstrap':1, 'Gentoo':2}
 def target_encode(val):
   return target_mapper[val]
 
+y = y_raw.apply(target_encode)
+y
+
 with st.expander('Input features'):
   st.write('**input penguin**')
   st.dataframe(input_df)
