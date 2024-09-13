2705ec7850ad8fdd99b4ddf97876192ed6ec3628
Shreya Doodipala <95224610+Shreyadoodipala@users.noreply.github.com>
2024-09-13 19:44:11 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title(':penguin: Penguin Prediction ML App')
 st.info('This is a ML app.')
+
+with st.expander('Data'):
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df.head()
