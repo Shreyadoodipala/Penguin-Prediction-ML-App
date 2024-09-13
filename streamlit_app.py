ecacdd94b4d28b48a7fb536dd3a67d2360bd4a88
Shreya Doodipala <95224610+Shreyadoodipala@users.noreply.github.com>
2024-09-13 23:10:03 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,4 +74,5 @@ pred_proba = RFC.predict_proba(input_row)
 
 df_prediction_proba = pd.DataFrame(pred_proba)
 df_prediction_proba.columns = ['Adelie', 'Chinstrap', 'Gentoo']
+df_prediction_proba.rename(columns={0: 'Adelie', 1: 'Chinstrap', 2: 'Gentoo'})
 df_prediction_proba
