8b4874ea6de219721124d663757899f1d7a917fe
Shreya Doodipala <95224610+Shreyadoodipala@users.noreply.github.com>
2024-09-13 20:48:07 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,14 @@ st.title(':penguin: Penguin Prediction ML App')
 st.info('This is a ML app.')
 
 with st.expander('Data'):
+  st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-  st.dataframe(df.head())
+  st.dataframe(df.head(10))
+
+  st.write('**X**')
+  X = df.drop['species', axis=1]
+  st.dataframe(X.head(10))
+
+  st.write('**Y**')
+  y = df.species
+  st.dataframe(y.head(10))
